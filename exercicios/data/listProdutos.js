b9ed231ab91fefe22982e142f93a39e1672b1449
Guilherme <guilhermemascarenhas21g@gmail.com>
2023-10-26 15:42:52 -0300

    add exercicios

---
+++ exercicios/data/listProdutos.js
@@ -0,0 +1,9 @@
+import Produto from "../models/produto";
+
+export default  [
+    new Produto(1, "MacBook", 8423),
+    new Produto(2, "Iphone 15", 4370),
+    new Produto(3, "Playstaion 5", 3460),
+    new Produto(4, "Xbox series X", 3320)
+]
+
